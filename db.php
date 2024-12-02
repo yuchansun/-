523942a3caf83399413b7879742b05d05707ec62
Nico sun <nicosun0515gmail.com>
2024-12-02 20:55:07 +0800

    add db.php

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost:8889";
+
+$dbname = "practice";
+
+$dbUsername = "root";
+
+$dbPassword = "";
+
+
+
+$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbname);
+
+
+
+?>
