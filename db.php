3138f8ec3256e303b66d98564046face7264f57e
Jenny Chu <jennychu10022004@gmail.com>
2024-12-02 21:15:31 +0800

    777

--- db.php
+++ db.php
@@ -12,6 +12,14 @@ $dbPassword = "";
 
 $conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbname);
 
+if ($conn->connect_error) {
+    die("連線失敗: " . $conn->connect_error);
+}
+echo "成功連線到資料庫";
+
+
+?>
+
 
 ?>
 <H1>hi</H1>
