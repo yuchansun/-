5e6dc2c8198b0d84ba2c9018d461aaa9dbc2af44
Jenny Chu <jennychu10022004@gmail.com>
2024-12-18 16:30:09 +0800

    期一搧了header一巴掌

--- 活動資料.php
+++ 活動資料.php
@@ -1,46 +1,3 @@
-<!DOCTYPE html>
-<html lang="zh-Hant">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>活動資料</title>
-    <!-- 引入 Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        th, td {
-            padding: 15px;
-            text-align: left;
-            border-bottom: 1px solid #ddd;
-        }
-        button {
-            padding: 8px 16px;
-            background-color: #6C757D;
-            color: white;
-            border: none;
-            cursor: pointer;
-            border-radius: 4px;
-        }
-        button:hover {
-            background-color: #45a049;
-        }
-        .collapse-content {
-            margin-top: 10px;
-            padding: 15px;
-            background-color: #f9f9f9;
-            border: 1px solid #ddd;
-        }
-        .btn.btn-secondary{
-            background-color: #6C757D;
-  
-        }
-        
-    </style>
-</head>
-<body>
 
 <?php
 require_once "header.php";
@@ -152,6 +109,3 @@ $conn->close();
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
-
-</body>
-</html>
